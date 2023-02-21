fba0f336e547c72f3a88756c1de8fb6a83d0ea5d
sabuj-khan <shobuj.khan1231@gmail.com>
2023-02-22 03:33:17 +0600

    key-encript-decript

--- encript.php
+++ encript.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 $original = "abcdefghijklmnopqrstuvwxyz0123456789";
 $key = "x0n3uoysf4tiac9vj2lr6dmpzkb85h1qwge7";
 $message = "Amar sonar bangla, ami tomay valobasi.";
-$message = strtolower($message);
+$message = strtolower( $message );
 $result = '';
-$length = strlen($message);
+$length = strlen( $message );
 
-for($i=0; $i<$length; $i++){
+for ( $i = 0; $i < $length; $i++ ) {
     $character = $message[$i];
-    $position = strpos($original, $character);
-    if($position !== false){
+    $position = strpos( $original, $character );
+    if ( $position !== false ) {
         $replacement = $key[$position];
-    }else{
+    } else {
         $replacement = $character;
     }
-    $result = $result.$replacement;
+    $result = $result . $replacement;
 }
 
 echo $result;
