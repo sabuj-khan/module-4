228ba3b4f18e30641834026dd54c943f83e8b096
sabuj-khan <shobuj.khan1231@gmail.com>
2023-02-22 03:31:58 +0600

    key-encript-decript

--- decript.php
+++ decript.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 $original = "abcdefghijklmnopqrstuvwxyz0123456789";
 $key = "x0n3uoysf4tiac9vj2lr6dmpzkb85h1qwge7";
 $message = "xax2 l9cx2 0xcyix, xaf r9axz dxi90xlf.";
-$message = strtolower($message);
+$message = strtolower( $message );
 $result = '';
-$length = strlen($message);
+$length = strlen( $message );
 
-for($i=0; $i<$length; $i++){
+for ( $i = 0; $i < $length; $i++ ) {
     $character = $message[$i];
-    $position = strpos($key, $character);
-    if($position !== false){
+    $position = strpos( $key, $character );
+    if ( $position !== false ) {
         $replacement = $original[$position];
-    }else{
+    } else {
         $replacement = $character;
     }
-    $result = $result.$replacement;
+    $result = $result . $replacement;
 }
 
 echo $result;
--- key.php
+++ key.php
@@ -2,6 +2,6 @@
 $original = "abcdefghijklmnopqrstuvwxyz0123456789";
 $key = "x0n3uoysf4tiac9vj2lr6dmpzkb85h1qwge7";
 $parts = str_split( $original );
-shuffle( $parts );
+//shuffle( $parts );
 $key = join( '', $parts );
 echo $key;
